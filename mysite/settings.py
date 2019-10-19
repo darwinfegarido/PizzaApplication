f3ab8d765e9bcbb021c68360362ea14f538c9bc6
Darwin Fegarido <darwinfegarido@gmail.com>
2019-10-20 05:36:33 +0800

    Update

--- mysite/settings.py
+++ mysite/settings.py
@@ -74,10 +74,6 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # https://docs.djangoproject.com/en/dev/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    },
     'production': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'rorounifix$pizzaapp',
